e0524b1193cf819c30dced0d0f0049e1cbd59efd
Sajib <p2385553@gmail.com>
2024-02-16 10:22:40 +0600

    added files

--- main.js
+++ main.js
@@ -7,5 +7,12 @@
           loop: true
           
         });
+
+        function toggleMenu() {
+          const menu = document.querySelector(".menu-links");
+          const icon = document.querySelector(".hamburger-icon");
+          menu.classList.toggle("open");
+          icon.classList.toggle("open");
+        }
         
     
